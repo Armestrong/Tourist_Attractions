db98434394d0d1efb2a1f858ebb5c5c817b35f05
Armestrong <Armestrongcalazans@hotmail.com>
2021-04-12 15:25:35 -0300

    implemented the fields on serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import TouristAttraction
+from attractions.api.serializers import ComplementAttractionSerializer
+from comment.api.serializers import CommentSerializer
+from address.api.serializers import AddressSerializer
+from review.api.serializers import ReviewSerializers
 
 
 # Serializers define the API representation.
 class TouristAttractionSerializer(ModelSerializer):
+    attractions = ComplementAttractionSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    address = AddressSerializer()
+    reviews = ReviewSerializers(many=True)
+
     class Meta:
         model = TouristAttraction
-        fields = ['id', 'name', 'description', 'approved', 'image']
+        fields = ['id', 'name', 'description', 'approved', 'minimal_age', 'attractions', 'comments', 'reviews',
+                  'address', 'image']
