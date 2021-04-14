cdb676907b41c6eb8f87dbf6940c53db5e2f0ea4
Armestrong <Armestrongcalazans@hotmail.com>
2021-04-14 19:16:17 -0300

    Deploy API

--- tourist_attractions/settings/base.py
+++ tourist_attractions/settings/base.py
@@ -132,6 +132,8 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
 }
 
 # Para guardar medias
